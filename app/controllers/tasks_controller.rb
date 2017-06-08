e8a9e46eb218535fff8f718c73bbaa0a42f7fc98
Kunihiko Shigematsu <monolith1978@gmail.com>
2017-06-08 13:45:16 +0000

    3rd

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,7 +14,7 @@ class TasksController < ApplicationController
       flash[:success] = 'Taskが正常に投稿されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Taskが投稿されませんでした。'
+      # flash.now[:danger] = 'Taskが投稿されませんでした。'
       render :new
     end
   end
